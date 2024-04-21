24a287491b42fe6704f28d05d9efb77d12efa92e
Andreea <c.1andreea874@gmail.com>
2024-04-21 22:39:29 +0300

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+       
+        <p>
+         <h1>
+          Hello world!
+         </h1>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
